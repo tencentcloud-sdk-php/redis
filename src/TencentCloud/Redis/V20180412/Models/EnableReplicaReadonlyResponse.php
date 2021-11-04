0c5353082816825c3d07373d9e7d7f7048d20b5f
tencentcloudapi <tencentcloudapi@tencent.com>
2021-11-04 08:48:37 +0800

    release 3.0.513

--- src/TencentCloud/Redis/V20180412/Models/EnableReplicaReadonlyResponse.php
+++ src/TencentCloud/Redis/V20180412/Models/EnableReplicaReadonlyResponse.php
@@ -20,17 +20,30 @@ use TencentCloud\Common\AbstractModel;
 /**
  * EnableReplicaReadonly返回参数结构体
  *
- * @method string getStatus() 获取错误：ERROR，正确OK。
- * @method void setStatus(string $Status) 设置错误：ERROR，正确OK。
+ * @method string getStatus() 获取错误：ERROR，正确OK（已废弃）
+注意：此字段可能返回 null，表示取不到有效值。
+ * @method void setStatus(string $Status) 设置错误：ERROR，正确OK（已废弃）
+注意：此字段可能返回 null，表示取不到有效值。
+ * @method integer getTaskId() 获取任务ID
+注意：此字段可能返回 null，表示取不到有效值。
+ * @method void setTaskId(integer $TaskId) 设置任务ID
+注意：此字段可能返回 null，表示取不到有效值。
  * @method string getRequestId() 获取唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  */
 class EnableReplicaReadonlyResponse extends AbstractModel
 {
     /**
-     * @var string 错误：ERROR，正确OK。
+     * @var string 错误：ERROR，正确OK（已废弃）
+注意：此字段可能返回 null，表示取不到有效值。
      */
     public $Status;
+
+    /**
+     * @var integer 任务ID
+注意：此字段可能返回 null，表示取不到有效值。
+     */
+    public $TaskId;
 
     /**
      * @var string 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
@@ -38,7 +51,10 @@ class EnableReplicaReadonlyResponse extends AbstractModel
     public $RequestId;
 
     /**
-     * @param string $Status 错误：ERROR，正确OK。
+     * @param string $Status 错误：ERROR，正确OK（已废弃）
+注意：此字段可能返回 null，表示取不到有效值。
+     * @param integer $TaskId 任务ID
+注意：此字段可能返回 null，表示取不到有效值。
      * @param string $RequestId 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -56,6 +72,10 @@ class EnableReplicaReadonlyResponse extends AbstractModel
         }
         if (array_key_exists("Status",$param) and $param["Status"] !== null) {
             $this->Status = $param["Status"];
+        }
+
+        if (array_key_exists("TaskId",$param) and $param["TaskId"] !== null) {
+            $this->TaskId = $param["TaskId"];
         }
 
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
